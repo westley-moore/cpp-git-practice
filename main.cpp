1466749b81f0fec71d0d14ceb344ccdb1aea9393
Westley <westleyhm@gmail.com>
2026-08-26 11:37:38 -0700

    add another message

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello from Git!" << std::endl;
     std::cout << "This line was added by Student B." << std::endl;
+    std::cout << "Another change from Student A." << std::endl;
     return 0;
 }
